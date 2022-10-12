048a81d348e13514708f969ec6406014a219c975
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-10-11 22:27:01 -0300

    Finalizando GrayContainer component

---
+++ src/components/GrayContainer/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const GrayBackground = styled.div`
+  background-color: #efefef;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 0 5% 25px 5%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
