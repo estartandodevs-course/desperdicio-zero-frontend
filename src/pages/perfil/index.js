3d658fb48d78791cc9ac0afbdac9340c0716ab98
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-10-11 22:33:37 -0300

    Finalizando página de perfil

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Header from '../../components/Header';
+import PageTitle from '../../components/PageTitle';
+import PrimaryButton from '../../components/PrimaryButton';
+import UserCard from '../../components/UserCard/index';
+import { MdAddCircleOutline } from 'react-icons/md';
+import { PerfilContainer } from './style';
+import MenuList from '../../components/MenuList';
+import { GrayBackground } from '../../components/GrayContainer/style';
+import WasteCounter from '../../components/WasteCounter';
 
 function Perfil() {
   return (
-    <>
-      <h1>Perfil</h1>
-      <h1>Usuário</h1>
-    </>
+    <PerfilContainer>
+      <Header />
+      <PageTitle title={'Dados do Perfil'} />
+      <GrayBackground>
+        <UserCard />
+        <PrimaryButton icon={<MdAddCircleOutline size={34} />} text="Adicionar itens" />
+        <MenuList />
+        <WasteCounter />
+      </GrayBackground>
+    </PerfilContainer>
   );
 }
 
