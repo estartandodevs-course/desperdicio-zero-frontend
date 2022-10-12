ee01641f3f2111e96fa22cd3f8ea1ccc94326b72
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-10-11 22:33:06 -0300

    Ajustando MSW e incluindo mock do contador de desperdício

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,14 +1,23 @@
-import { rest } from "msw";
+import { rest } from 'msw';
 
 export const handlers = [
   rest.get('meuendpoint.com.br/usuario', (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
-        nome: "Marcela Fernanda",
-        idade: 35
+        nome: 'Marcela Fernanda',
+        idade: 35,
+        imagem: 'https://cdn.mindminers.com/blog/uploads/2021/05/Dani-Almeida_auto_x1.png',
       })
-    )
-  })
+    );
+  }),
 
-]
+  rest.get('meuendpoint.com.br/valordesperdicado', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        valor: 124.99,
+      })
+    );
+  }),
+];
