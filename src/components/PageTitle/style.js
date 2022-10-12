a2ac5682f727f8878d2bbeff812f46ffbfe13d1d
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-10-11 22:28:53 -0300

    Finalizando componente de título das páginas

--- src/components/PageTitle/style.js
+++ src/components/PageTitle/style.js
@@ -4,9 +4,10 @@ export const TitleContainer = styled.div`
   background-color: #efefef;
   font-weight: 700;
   border-radius: 15px;
-  padding: 10px 15px;
-  margin: 25px 25%;
+  padding: 10px 25px;
+  margin: 25px 0;
   text-align: center;
+  font-size: 24px;
 
   p {
     margin: 0;
