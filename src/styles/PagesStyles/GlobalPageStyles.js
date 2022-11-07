89dbc46c9674f64552d14f552830f127f4947b80
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-11-07 17:55:45 -0300

    feat: alterando estilo de links

--- src/styles/PagesStyles/GlobalPageStyles.js
+++ src/styles/PagesStyles/GlobalPageStyles.js
@@ -4,6 +4,12 @@ export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  margin-bottom: 71px;
+  margin-bottom: 96px;
   margin-top: 60px;
+
+  a {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
 `;
