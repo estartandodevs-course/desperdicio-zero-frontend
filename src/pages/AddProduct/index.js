9352c13734a889e22dc65eba8f684584845a57d0
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-11-07 17:47:37 -0300

    feat: Inclui componentes para cadastro de produto

--- src/pages/AddProduct/index.js
+++ src/pages/AddProduct/index.js
@@ -2,31 +2,64 @@ import React from 'react';
 import NavBar from '../../components/NavBar';
 import Header from '../../components/Header';
 import PageTitle from '../../components/PageTitle';
-// import { ReactComponent as NomeIcon } from '../../assets/icons/nome-icon.svg';
-// import { ReactComponent as DataDeValidadeIcon } from '../../assets/icons/data-de-validade-icon.svg';
 import { Container } from '../../styles/PagesStyles/GlobalPageStyles';
+import FoodForm from '../../components/FoodForm';
+import { ReactComponent as NomeIcon } from '../../assets/icons/nome-icon.svg';
+import { ReactComponent as DataDeValidadeIcon } from '../../assets/icons/data-de-validade-icon.svg';
 
 function AddProduct() {
-  // const inputs = [
-  //   {
-  //     name: 'Nome',
-  //     icon: NomeIcon,
-  //     type: 'text',
-  //   },
-  //   {
-  //     name: 'Data de validade',
-  //     icon: DataDeValidadeIcon,
-  //     type: 'date',
-  //   },
-  // ];
+  const inputs = [
+    {
+      name: 'Nome',
+      type: 'text',
+      icon: NomeIcon,
+    },
+    {
+      name: 'Data de validade',
+      type: 'date',
+      icon: DataDeValidadeIcon,
+    },
+    {
+      name: 'Prazo para consumo',
+      type: 'date',
+      icon: NomeIcon,
+    },
+    {
+      name: 'Local',
+      type: 'select',
+      icon: NomeIcon,
+      options: ['Geladeira', 'Freezer', 'Armário'],
+    },
+    {
+      name: 'Preço',
+      type: 'number',
+      icon: DataDeValidadeIcon,
+    },
+    {
+      name: 'Unidade de medida',
+      type: 'select',
+      icon: NomeIcon,
+      options: ['mm', 'g', 'kg', 'ml', 'l', 'und', 'lb', 'oz', 'gal', 'qt', 'fl. oz'],
+    },
+    {
+      name: 'Categorias',
+      type: 'select',
+      icon: NomeIcon,
+      options: [
+        'Frutas e verduras',
+        'Carnes e frios',
+        'Bebidas e outros produtos',
+        'Grão, cereais e farinhas',
+        'Outros',
+      ],
+    },
+  ];
 
   return (
     <Container>
       <Header />
       <PageTitle title={'Cadastro de alimentos'} />
-      {/* {inputs.map((item, index) => {
-        return <input type={item.type} placeholder={item.name} key={index} />;
-      })} */}
+      <FoodForm inputs={inputs} />
       <NavBar />
     </Container>
   );
