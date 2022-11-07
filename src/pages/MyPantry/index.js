8365e7f1a9101a3f6d595c52a6253a917f128b70
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-11-07 17:53:59 -0300

    Cria página de dispensa

--- src/pages/MyPantry/index.js
+++ src/pages/MyPantry/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from '../../components/Header';
 import NavBar from '../../components/NavBar';
+import PageTitle from '../../components/PageTitle';
+import ProductList from '../../components/ProductList';
 import { Container } from '../../styles/PagesStyles/GlobalPageStyles';
 
 function MyPantry() {
   return (
     <Container>
-      <p>Minha dispensa</p>
+      <Header />
+      <PageTitle title={'Minha dispensa'} />
+      <ProductList />
       <NavBar />
     </Container>
   );
